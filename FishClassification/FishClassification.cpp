2112dfd375fcf69e342c469cf17d8bdb70d2c664
Steven Hickson <me@stevenhickson.com>
2013-10-23 15:16:04 -0400

    Added code for color hist and HoGs. Still needs to be edited alot.

--- FishClassification/FishClassification.cpp
+++ FishClassification/FishClassification.cpp
@@ -33,19 +33,19 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 		else
 		{
 			try {
-			// Number of clusters for building BOW vocabulary from SURF features
-			int clusters = 1000;    
-			categorizer c(argv[1], clusters);
-			if(atoi(argv[2]) == 0) {
-				c.build_vocab();
-				c.train_classifiers();
-			} else {
-				c.load_vocab();
-			}
+				// Number of clusters for building BOW vocabulary from SURF features
+				int clusters = 1000;    
+				categorizer c(argv[1], clusters);
+				if(atoi(argv[2]) == 0) {
+					c.build_vocab();
+					c.train_classifiers();
+				} else {
+					c.load_vocab();
+				}
 
-			//VideoCapture cap(0);
-			namedWindow("Detected object");
-			c.categorize();
+				//VideoCapture cap(0);
+				namedWindow("Detected object");
+				c.categorize();
 			} catch(cv::Exception &e) {
 				printf("Error: %s\n", e.what());
 			}
@@ -62,6 +62,36 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 	return nRetCode;
 }
 
+inline void ComputeHistogram(Mat &img, HistInfo &histInfo, Mat *hist) {
+	Mat hist_base;
+	calcHist( &img, 1, histInfo.channels, Mat(), hist_base, 2, histInfo.histSize, histInfo.ranges, true, false );
+	normalize( hist_base, hist_base, 0, 1, NORM_MINMAX, -1, Mat() );
+	int tmp = hist_base.channels();
+	*hist = Mat(1,hist_base.rows * hist_base.cols,hist_base.type());
+	MatIterator_<float> pI = hist_base.begin<float>(), pO = hist->begin<float>(), pEnd = hist_base.end<float>();
+	while(pI != pEnd) {
+		*pO++ = *pI++;
+	}
+	//for displaying the histogram
+	/*double maxVal=0;
+    minMaxLoc(hist_base, 0, &maxVal, 0, 0);
+	Mat histImg = Mat::zeros( histInfo.s_bins*10,  histInfo.h_bins*10, CV_8UC3);
+	for( int h = 0; h < histInfo.h_bins; h++ )
+		for( int s = 0; s < histInfo.s_bins; s++ )
+		{
+			float binVal = hist_base.at<float>(h, s);
+			int intensity = cvRound(binVal*255/maxVal);
+			rectangle( histImg, Point(h*10, s*10),
+				Point( (h+1)*10 - 1, (s+1)*10 - 1),
+				Scalar::all(intensity),
+				CV_FILLED );
+		}
+		namedWindow( "H-S Histogram", 1 );
+		imshow( "H-S Histogram", histImg );
+		cvWaitKey();
+		*/
+}
+
 string categorizer::remove_extension(string full) {
 	int last_idx = full.find_last_of(".");
 	string name = full.substr(0, last_idx);
@@ -73,11 +103,11 @@ categorizer::categorizer(string direc, int _clusters) {
 	// Initialize pointers to all the feature detectors and descriptor extractors
 	featureDetector = (new SurfFeatureDetector());
 	descriptorExtractor = (new SurfDescriptorExtractor());
-	//hogDescriptor = (new HOGDescriptor());
+	hogDescriptor = (new HOGDescriptor());
 	bowtrainer = (new BOWKMeansTrainer(clusters));
 	descriptorMatcher = (new FlannBasedMatcher());
 	bowDescriptorExtractor = (new BOWImgDescriptorExtractor(descriptorExtractor, descriptorMatcher));
-	
+
 
 	//set up folders
 	template_folder = direc + "templates\\";
@@ -122,7 +152,7 @@ void categorizer::make_train_set() {
 			//Mat downsized;
 			//pyrDown(tmp,downsized,Size(tmp.cols / 2, tmp.rows /2));
 			// Make a pair of string and Mat to insert into multimap
-			pair<string, Mat> p(category, imread(filename, CV_LOAD_IMAGE_GRAYSCALE));
+			pair<string, Mat> p(category, imread(filename));
 			train_set.insert(p);
 		}
 	}
@@ -135,33 +165,43 @@ void categorizer::make_pos_neg() {
 	// Iterate through the whole training set of images
 	for(multimap<string, Mat>::iterator i = train_set.begin(); i != train_set.end(); i++) {
 		// Category name is the first element of each entry in train_set
-		string category = (*i).first;
+		string category = i->first;
 		// Training image is the second elemnt
-		Mat im = (*i).second, feat;
-
+		Mat im = i->second, feat, im_g, im_hsv, hist;
+		cvtColor(im,im_g,CV_BGR2GRAY);
+		cvtColor(im,im_hsv, CV_BGR2HSV);
 		// Detect keypoints, get the image BOW descriptor
 		vector<KeyPoint> kp;
-		featureDetector->detect(im, kp);
-		
-		bowDescriptorExtractor->compute(im, kp, feat);
-
+		featureDetector->detect(im_g, kp);
+		bowDescriptorExtractor->compute(im_g, kp, feat);
+		//compute histogram
+		ComputeHistogram(im_hsv,histInfo,&hist);
+		//compute HoG descriptors
+		vector<float> descriptors;
+		hogDescriptor->compute(im_g, descriptors);
+		Mat hogs = Mat(descriptors);
 		// Mats to hold the positive and negative training data for current category
 		Mat pos, neg;
 		for(int cat_index = 0; cat_index < categories; cat_index++) {
 			string check_category = category_names[cat_index];
 			// Add BOW feature as positive sample for current category ...
-			if(check_category.compare(category) == 0)
-				positive_data[check_category].push_back(feat);
-			//... and negative sample for all other categories
-			else
-				negative_data[check_category].push_back(feat);
+			if(check_category.compare(category) == 0) {
+				positive_surf[check_category].push_back(feat);
+				/*positive_hist[check_category].push_back(hist);
+				positive_hog[check_category].push_back(hogs);*/
+				//... and negative sample for all other categories
+			} else {
+				negative_surf[check_category].push_back(feat);
+				/*negative_hist[check_category].push_back(hist);
+				negative_hog[check_category].push_back(hogs);*/
+			}
 		}
 	}
 
 	// Debug message
 	for(int i = 0; i < categories; i++) {
 		string category = category_names[i];
-		cout << "Category " << category << ": " << positive_data[category].rows << " Positives, " << negative_data[category].rows << " Negatives" << endl;
+		cout << "Category " << category << ": " << positive_surf[category].rows << " Positives, " << negative_surf[category].rows << " Negatives" << endl;
 	}
 }
 
@@ -173,7 +213,7 @@ void categorizer::build_vocab() {
 	Mat templ, desc;
 	for(map<string, Mat>::iterator i = templates.begin(); i != templates.end(); i++) {
 		vector<KeyPoint> kp;
-		templ = (*i).second;
+		templ = i->second;
 		featureDetector->detect(templ, kp);
 		descriptorExtractor->compute(templ, kp, desc);
 		vocab_descriptors.push_back(desc);
@@ -207,7 +247,11 @@ void categorizer::load_vocab() {
 	for(int i = 0; i < categories; i++) {
 		string category = category_names[i];
 		string svm_filename = string(vocab_folder) + category + string("SVM.xml");
-		svms[category].load(svm_filename.c_str());
+		svms_surf[category].load(svm_filename.c_str());
+		/*svm_filename = string(vocab_folder) + category + string("SVM2.xml");
+		svms_hist[category].load(svm_filename.c_str());
+		svm_filename = string(vocab_folder) + category + string("SVM3.xml");
+		svms_hog[category].load(svm_filename.c_str());*/
 	}
 }
 
@@ -221,22 +265,45 @@ void categorizer::train_classifiers() {
 		string category = category_names[i];
 
 		// Postive training data has labels 1
-		Mat train_data = positive_data[category], train_labels = Mat::ones(train_data.rows, 1, CV_32S);
+		Mat train_data_surf = positive_surf[category], train_labels_surf = Mat::ones(train_data_surf.rows, 1, CV_32S);
 		// Negative training data has labels 0
-		train_data.push_back(negative_data[category]);
-		Mat m = Mat::zeros(negative_data[category].rows, 1, CV_32S);
-		train_labels.push_back(m);
+		train_data_surf.push_back(negative_surf[category]);
+		Mat m = Mat::zeros(negative_surf[category].rows, 1, CV_32S);
+		train_labels_surf.push_back(m);
+
+		//Mat train_data_hist = positive_hist[category], train_labels_hist = Mat::ones(train_data_hist.rows, 1, CV_32S);
+		//// Negative training data has labels 0
+		//train_data_hist.push_back(negative_hist[category]);
+		//m = Mat::zeros(negative_hist[category].rows, 1, CV_32S);
+		//train_labels_hist.push_back(m);
+
+		//Mat train_data_hog = positive_hog[category], train_labels_hog = Mat::ones(train_data_hog.rows, 1, CV_32S);
+		//// Negative training data has labels 0
+		//train_data_hog.push_back(negative_hog[category]);
+		//m = Mat::zeros(negative_hog[category].rows, 1, CV_32S);
+		//train_labels_hog.push_back(m);
 
 		// Train SVM!
-		svms[category].train(train_data, train_labels);
+		svms_surf[category].train(train_data_surf, train_labels_surf);
+		/*svms_hist[category].train(train_data_hist, train_labels_hist);
+		svms_hog[category].train(train_data_hog, train_labels_hog);*/
 
 		// Save SVM to file for possible reuse
 		string svm_filename = string(vocab_folder) + category + string("SVM.xml");
-		svms[category].save(svm_filename.c_str());
+		svms_surf[category].save(svm_filename.c_str());
+		/*svm_filename = string(vocab_folder) + category + string("SVM2.xml");
+		svms_hist[category].save(svm_filename.c_str());
+		svm_filename = string(vocab_folder) + category + string("SVM3.xml");
+		svms_hog[category].save(svm_filename.c_str());*/
 
 		cout << "Trained and saved SVM for category " << category << endl;
 	}
-
+	positive_surf.clear();
+	negative_surf.clear();
+	/*positive_hist.clear();
+	negative_hist.clear();
+	positive_hog.clear();
+	negative_hog.clear();*/
 }
 
 void categorizer::categorize(VideoCapture cap) {
@@ -261,7 +328,7 @@ void categorizer::categorize(VideoCapture cap) {
 		string predicted_category;
 		for(int i = 0; i < categories; i++) {
 			string category = category_names[i];
-			float prediction = svms[category].predict(test, true);
+			float prediction = svms_surf[category].predict(test, true);
 			//cout << category << " " << prediction << " ";
 			if(prediction < best_score) {
 				best_score = prediction;
@@ -280,7 +347,7 @@ void categorizer::categorize() {
 	namedWindow("Image");
 
 	for(directory_iterator i(test_folder), end_iter; i != end_iter; i++) {
-		Mat frame, frame_small, frame_g;
+		Mat frame, frame_small, frame_g, frame_hsv;
 		// Prepend full path to the file name so we can imread() it
 		string filename = string(test_folder) + i->path().filename().string();
 		cout << "Opening file: " << filename << endl;
@@ -289,19 +356,26 @@ void categorizer::categorize() {
 		resize(frame, frame_small, Size(), 0.25f, 0.25f);
 		//pyrDown(frame, frame_small, Size(frame.cols / 2, frame.rows / 2));
 		cvtColor(frame_small, frame_g, CV_BGR2GRAY);
+		/*cvtColor(frame_small, frame_hsv, CV_BGR2HSV);*/
 		imshow("Image", frame_small);
 		// Extract frame BOW descriptor
 		vector<KeyPoint> kp;
-		Mat test;
+		Mat test_surf, test_hist;
 		featureDetector->detect(frame_g, kp);
-		bowDescriptorExtractor->compute(frame_g, kp, test);
+		bowDescriptorExtractor->compute(frame_g, kp, test_surf);
 
+		/*ComputeHistogram(frame_hsv,histInfo,&test_hist);
+
+		vector<float> test_hog;
+		hogDescriptor->compute(frame_g, test_hog);*/
 		// Predict using SVMs for all catgories, choose the prediction with the most negative signed distance measure
-		float best_score = 777;
+		float best_score = 777, best_score2 = 777, best_score3 = 777;
 		string predicted_category;
 		for(int i = 0; i < categories; i++) {
 			string category = category_names[i];
-			float prediction = svms[category].predict(test, true);
+			float prediction = svms_surf[category].predict(test_surf, true);
+			/*float prediction2 = svms_hist[category].predict(test_hist, true);
+			float prediction3 = svms_hog[category].predict(Mat(test_hog), true);*/
 			cout << category << " " << prediction << " ";
 			if(prediction < best_score && prediction < THRESH) {
 				best_score = prediction;
@@ -311,9 +385,10 @@ void categorizer::categorize() {
 		cout << endl;
 
 		// Pull up the object template for the detected category and show it in a separate window
-		if(predicted_category.empty())
+		if(predicted_category.empty()) {
 			cout << "Couldn't find a match!\n" << endl;
-		else
+			imshow("Detected object", NULL);
+		} else
 			imshow("Detected object", objects[predicted_category]);
 		waitKey();
 	}
--- FishClassification/FishClassification.h
+++ FishClassification/FishClassification.h
@@ -12,35 +12,60 @@ using namespace cv;
 using namespace std;
 using namespace boost::filesystem;
 
-class categorizer {
-    private:
-        map<string, Mat> templates, objects, positive_data, negative_data; //maps from category names to data
-        multimap<string, Mat> train_set; //training images, mapped by category name
-        map<string, CvSVM> svms; //trained SVMs, mapped by category name
-        vector<string> category_names; //names of the categories found in TRAIN_FOLDER
-        int categories; //number of categories
-        int clusters; //number of clusters for SURF features to build vocabulary
-        Mat vocab; //vocabulary
-		string train_folder, test_folder, template_folder, vocab_folder;
-    
-        // Feature detectors and descriptor extractors
-        Ptr<FeatureDetector> featureDetector;
-        Ptr<DescriptorExtractor> descriptorExtractor;
-        Ptr<BOWKMeansTrainer> bowtrainer;
-        Ptr<BOWImgDescriptorExtractor> bowDescriptorExtractor;
-        Ptr<DescriptorMatcher> descriptorMatcher;
-		//Ptr<HOGDescriptor> hogDescriptor;
+static class HistInfo {
+public:
+	/// Using 30 bins for hue and 32 for saturation
+	static const int h_bins = 40; 
+	static const int s_bins = 25;
+	int histSize[2];
+	// hue varies from 0 to 256, saturation from 0 to 180
+	float h_ranges[2];
+	float s_ranges[2];
+	const float* ranges[2];
+	// Use the o-th and 1-st channels
+	int channels[];
 
-        void make_train_set(); //function to build the training set multimap
-        void make_pos_neg(); //function to extract BOW features from training images and organize them into positive and negative samples 
-        string remove_extension(string); //function to remove extension from file name, used for organizing templates into categories
-    public:
-        categorizer(string direc, int _clusters); //constructor
-        void build_vocab(); //function to build the BOW vocabulary
-		void load_vocab(); //function to load the BOW vocabulary and classifiers
-        void train_classifiers(); //function to train the one-vs-all SVM classifiers for all categories
-        void categorize(VideoCapture); //function to perform real-time object categorization on camera frames
-        void categorize(); //function to perform real-time object categorization on saved frames
+	HistInfo() {
+		histSize[0] = h_bins; histSize[1] = s_bins;
+		// hue varies from 0 to 256, saturation from 0 to 180
+		h_ranges[0] = 0; h_ranges[1] = 256;
+		s_ranges[0] = 0; s_ranges[1] = 180;
+		ranges[0] = h_ranges; ranges[1] = s_ranges;
+		// Use the o-th and 1-st channels
+		channels[0] = 0; channels[1] = 1;
+	}
 };
 
-#define THRESH 0.9980f
+class categorizer {
+private:
+	map<string, Mat> templates, objects, positive_surf, negative_surf, positive_hist, negative_hist, positive_hog, negative_hog; //maps from category names to data
+	multimap<string, Mat> train_set; //training images, mapped by category name
+	map<string, CvSVM> svms_surf, svms_hist, svms_hog; //trained SVMs, mapped by category name
+	vector<string> category_names; //names of the categories found in TRAIN_FOLDER
+	int categories; //number of categories
+	int clusters; //number of clusters for SURF features to build vocabulary
+	Mat vocab; //vocabulary
+	string train_folder, test_folder, template_folder, vocab_folder;
+
+	// Feature detectors and descriptor extractors
+	Ptr<FeatureDetector> featureDetector;
+	Ptr<DescriptorExtractor> descriptorExtractor;
+	Ptr<BOWKMeansTrainer> bowtrainer;
+	Ptr<BOWImgDescriptorExtractor> bowDescriptorExtractor;
+	Ptr<DescriptorMatcher> descriptorMatcher;
+	Ptr<HOGDescriptor> hogDescriptor;
+	HistInfo histInfo;
+
+	void make_train_set(); //function to build the training set multimap
+	void make_pos_neg(); //function to extract BOW features from training images and organize them into positive and negative samples 
+	string remove_extension(string); //function to remove extension from file name, used for organizing templates into categories
+public:
+	categorizer(string direc, int _clusters); //constructor
+	void build_vocab(); //function to build the BOW vocabulary
+	void load_vocab(); //function to load the BOW vocabulary and classifiers
+	void train_classifiers(); //function to train the one-vs-all SVM classifiers for all categories
+	void categorize(VideoCapture); //function to perform real-time object categorization on camera frames
+	void categorize(); //function to perform real-time object categorization on saved frames
+};
+
+#define THRESH 0.9970f
